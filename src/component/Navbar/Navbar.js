e3f1a40df9620a1d66066b71885ba4f80253e1e6
LeeSeogMin <newmind68@hs.ac.kr>
2022-07-21 20:53:23 +0900

    f1

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -15,17 +15,20 @@ const Navbar = () => {
         );
     });
 
-    const handleClick = () => {
-        setClicked(!clicked);
-    };
+    // const handleClick = () => {
+    //     setClicked(!clicked);
+    // };
 
     return (
         <nav>
             <div className="logo">한신대 공공인재빅데이터융합학부</div>
-            <div className="menu-icon" onClick={handleClick}>
+            {/* <div className="menu-icon" onClick={handleClick}>
                 <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div>
-            <ul className={clicked ? 'menu-list' : 'menu-list close'}>
+            </div> */}
+            <ul
+                className={clicked ? 'menu-list' : 'menu-list close'}
+                onClick={() => setClicked(false)}
+            >
                 {menuList}
             </ul>
         </nav>
