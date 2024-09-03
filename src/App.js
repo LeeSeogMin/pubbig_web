a071c4bc6932c4f17e68430ad48a6bdd80926c78
LeeSeogMin <newmind68@hs.ac.kr>
2024-09-04 01:05:11 +0900

    MAJOR UPDATE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Navbar from './component/Navbar/Navbar';
 import Event from './component/Event/Event';
 import Home from './component/Hero/Home';
@@ -10,40 +12,43 @@ import Capston from './component/Capston/Capston';
 import Contact from './component/Contact/Contact';
 import Academy from './component/Academy/Academy';
 import './App.css';
-import Footer from './component/Footer';
 
-const App = () => {
+function App() {
     return (
-        <div className="container">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route
-                    path="/component/Major/Major"
-                    element={<Major />}
-                ></Route>
-                <Route path="/component/Prof/Prof" element={<Prof />}></Route>
-                <Route
-                    path="/component/Academy/Academy"
-                    element={<Academy />}
-                ></Route>
-                <Route path="/component/Lab/Lab" element={<Lab />}></Route>
-                <Route
-                    path="/component/Capston/Capston"
-                    element={<Capston />}
-                ></Route>
-                <Route
-                    path="/component/Event/Event"
-                    element={<Event />}
-                ></Route>
-                <Route
-                    path="/component/Contact/Contact"
-                    element={<Contact />}
-                ></Route>
-            </Routes>
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <div className="container">
+                    <Switch>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route
+                            path="/component/Major/Major"
+                            element={<Major />}
+                        ></Route>
+                        <Route path="/component/Prof/Prof" element={<Prof />}></Route>
+                        <Route
+                            path="/component/Academy/Academy"
+                            element={<Academy />}
+                        ></Route>
+                        <Route path="/component/Lab/Lab" element={<Lab />}></Route>
+                        <Route
+                            path="/component/Capston/Capston"
+                            element={<Capston />}
+                        ></Route>
+                        <Route
+                            path="/component/Event/Event"
+                            element={<Event />}
+                        ></Route>
+                        <Route
+                            path="/component/Contact/Contact"
+                            element={<Contact />}
+                        ></Route>
+                    </Switch>
+                </div>
+                <Footer />
+            </div>
+        </Router>
     );
-};
+}
 
 export default App;
